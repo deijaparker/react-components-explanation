bcbbcb82be8e001eb7a8f0b3d96eed6f489527ea
deijaparker <deijaparker1@outlook.com>
2024-09-17 16:45:28 -0700

    Add explanation for React components in comments

---
+++ react-components-explanation.js
@@ -0,0 +1,6 @@
+// Explanation of React Components
+// React components are the building blocks of a React application.
+// A component is a JavaScript function or class that optionally accepts inputs (called "props")
+// and returns a React element that describes how a section of the UI should appear.
+
+import React from "react"; // Import React library to use React features
