88ec0baaf6532280bbca88c7fdc43b3e69a1e2bc
deijaparker <deijaparker1@outlook.com>
2024-09-17 16:46:46 -0700

    Add explanation for functional components in comments

--- react-components-explanation.js
+++ react-components-explanation.js
@@ -4,3 +4,11 @@
 // and returns a React element that describes how a section of the UI should appear.
 
 import React from "react"; // Import React library to use React features
+
+// Functional Component
+// Functional components are JavaScript functions that return JSX (a syntax extension that allows mixing HTML with JavaScript).
+// They are ideal for simple components that do not require internal state or lifecycle methods.
+function Welcome(props) {
+  // Props are inputs passed to the component, in this case, `name`
+  return <h1>Hello, {props.name}!</h1>;
+}
